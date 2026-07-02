159829f2c61f3ac718555ede44ede6a0ee7fac73
Damix <158813145+Damix-hash@users.noreply.github.com>
2026-07-02 20:00:12 +0200

    Update move_to.py
    implemented delta so the drone can wrap across the map instead of flying 
    
    (get_world_size() > 1 instead of get_world_size() > get_world_size() - 1 until 1)

--- src/move_to.py
+++ src/move_to.py
@@ -1,16 +1,25 @@
-def move_to(y, x):
+def move_to(x=0, y=0):
+	
+	world_size = get_world_size()
+	
 	while True:
-		if (get_pos_y() < y):
-			move(North)
-		
-		elif (get_pos_y() > y):
-			move(South)
-			
-		if (get_pos_x() < x):
-			move(East)
-			
-		elif (get_pos_x() > x):
-			move(West)
-			
 		if ((get_pos_y() == y) and (get_pos_x() == x)):
 			break
+			
+		delta_x = (x - get_pos_x()) % world_size
+		delta_y = (y - get_pos_y()) % world_size
+		
+		if delta_x != 0:
+			if delta_x <= world_size // 2:
+				move(East)
+			else:
+				move(West)
+				
+		if delta_y != 0:
+			if delta_y <= world_size // 2:
+				move(North)
+			else:
+				move(South)
+
+if __name__ == "__main__":
+	move_to(10, 1)
